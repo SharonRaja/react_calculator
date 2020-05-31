34026bc7546f785ca8371c09f8a0253512407755
Sharon Raja <sharonrajad@gmail.com>
2020-05-31 20:23:16 +0530

    UI bug fix

--- src/DataContext.jsx
+++ src/DataContext.jsx
@@ -3,8 +3,8 @@ import React, { useState, createContext } from "react";
 export const DataContext = createContext();
 
 export const Dataprovider = (props) => {
-     const [inptxt, setinptxt] = useState("hi");
-     const [fultxt, setfultxt] = useState("history");
+     const [inptxt, setinptxt] = useState(" ");
+     const [fultxt, setfultxt] = useState("");
 
 
      return (
